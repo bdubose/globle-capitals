1a2e431da0db753eb2139baa334f5a8f09b8a918
the-abe-train <abraham.train@gmail.com>
2022-10-29 11:17:46 -0400

    Added local storage

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,56 @@
+import dayjs from "dayjs";
+import { Accessor, createEffect, createSignal, Setter } from "solid-js";
+
+// interface IStorage extends Object {
+//   day?: string;
+// }
+
+// type LocalStore = Record<string, any> & { day: string };
+
+type LocalStore<T> = { day: string } & T;
+
+function getStorageValue<T>(key: string, defaultValue?: LocalStore<T>) {
+  const saved = localStorage.getItem(key);
+  if (saved) {
+    return JSON.parse(saved) as LocalStore<T>;
+  } else if (defaultValue) {
+    return defaultValue;
+  } else {
+    throw new Error("Local storage error");
+  }
+}
+
+export function useLocalStorage<T>(
+  key: string,
+  defaultValue: T
+): [Accessor<T>, Setter<T>] {
+  const defaultWithDay = { ...defaultValue, day: dayjs().toString() };
+  const storedValue = getStorageValue<T>(key, defaultWithDay);
+  // const storedWithoutDay =
+
+  // Get expiration from local storage
+  // If expiration is later than now, don't edit expiration and change value
+  // If expiration is earlier than now, reset expiration and reset value
+
+  const [newValue, setNewValue] = createSignal(storedValue as T);
+
+  createEffect(() => {
+    const oldExpiration = dayjs(storedValue.day || "9999-12-31").endOf("day");
+    const newExpiration = dayjs().endOf("day");
+
+    console.log("Expiration:", oldExpiration.toString());
+    console.log("Now:", dayjs().toString());
+    console.log("New expiration:", newExpiration.toString());
+
+    if (oldExpiration.isAfter(dayjs())) {
+      console.log(`Saving to local storage "${key}".`);
+      const newData = { ...newValue(), day: newExpiration };
+      localStorage.setItem(key, JSON.stringify(newData));
+    } else {
+      console.log(`Local storage "${key}" expired.`);
+      localStorage.setItem(key, JSON.stringify(defaultValue));
+    }
+  });
+
+  return [newValue, setNewValue];
+}
--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,9 +1,19 @@
-import { createEffect, createSignal, lazy, Suspense } from "solid-js";
+import dayjs from "dayjs";
+import {
+  createEffect,
+  createSignal,
+  lazy,
+  onCleanup,
+  onMount,
+  Suspense,
+} from "solid-js";
 import { createStore } from "solid-js/store";
 import { computeDistanceBetween } from "spherical-geometry-js";
 import Guesser from "../components/Guesser";
 import List from "../components/List";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import { ans } from "../util/answer";
+import data from "../data/filter_cities.json";
 
 const GameGlobe = lazy(() => import("../components/globes/GameGlobe"));
 
@@ -11,10 +21,28 @@ export default function () {
   // Signals
   const [pov, setPov] = createSignal<Coords | null>(null);
   const [win, setWin] = createSignal(false);
+  const cities = data["data"] as City[];
+
+  // Local storage
+  const noCities = { cities: [] as string[] };
+  const [storedGuesses, storeGuesses] = useLocalStorage<typeof noCities>(
+    "guesses",
+    {
+      ...noCities,
+    }
+  );
+
+  const restoredGuesses = () =>
+    storedGuesses()
+      .cities.map((cityName, idx) => {
+        const city = cities.find((c) => c.city_ascii === cityName);
+        return { city, order: idx };
+      })
+      .filter((guess) => Boolean(guess.city)) as Guess[];
 
   // Stores
   const [guesses, setGuesses] = createStore({
-    cities: [] as Guess[],
+    cities: restoredGuesses() as Guess[],
     get sortedGuesses() {
       const copy = [...this.cities];
       return copy.sort((a, z) => {
@@ -37,6 +65,29 @@ export default function () {
     const winningGuess = guesses.cities.find(({ city }) => city.id === ans.id);
     if (winningGuess) setWin(true);
   });
+
+  // Stores guesses when new guess added
+  // TODO guesses not resetting on globle??
+  onMount(() => {
+    if (storedGuesses().cities.length === 0) {
+      setGuesses({ cities: [] });
+      return;
+    }
+  });
+
+  // Resets guesses when stored guesses expired
+  createEffect(() => {
+    // onCleanup(() => {
+    console.log("Running storing effect.");
+    const storable = guesses.cities.map((guess) => guess.city.city_ascii);
+    console.log("storable", storable);
+    storeGuesses({ cities: storable });
+  });
+  // createEffect(() => {
+  //   if (restoredGuesses().length === 0) {
+  //     setGuesses({ cities: [] });
+  //   }
+  // });
 
   return (
     <div>
