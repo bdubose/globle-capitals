cb296145e2d0401f7f8e7aa50213aba5f6fe1e97
the-abe-train <abraham.train@gmail.com>
2022-11-14 20:03:54 -0500

    Edited link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,7 @@
 import { A } from "@solidjs/router";
-import { getContext } from "../Context";
 import Icon from "./Icon";
 
 export default function Footer() {
-  const iconWidth = 14;
-  const { theme } = getContext();
-
   return (
     <footer class="pt-8 pb-4 text-xs flex items-end justify-between w-full">
       <span class="space-x-1 flex">
@@ -18,7 +14,7 @@ export default function Footer() {
           <Icon shape="twitter" size={14} />
         </a>
         <a
-          href="https://github.com/the-abe-train"
+          href="https://github.com/the-abe-train/globle-capitals"
           aria-label="Github"
           class="inline"
         >
